faf63e5d11a467fd3c84277c94adce1b767335d5
Alen Kalac <alenkalac87@gmail.com>
2017-02-19 19:38:05 +0000

    added font awesome, moved bootstrap to plugins

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,14 @@
 <?php 
 	$app->get('/', function() use($app) {
+		$uid = uniqid();
+		die($uid );
 		return $app['twig']->render('index.html.twig', []);
+	});
+
+	$app->get("/{id}", function ($id) use ($app) {
+
+		$uid = uniqid();
+		echo $uid;
+		die();
+
 	});
